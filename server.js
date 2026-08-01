f25fd273ec77d6901c248552f69ba262965a9b57
Sravani <59867313+SravaniSiriguppa@users.noreply.github.com>
2026-08-01 20:00:09 +0530

    feat: configure MongoDB connection

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 require("dotenv").config();
 
 const app = require("./src/app");
+const connectDB = require("./src/config/database");
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server is running on port ${PORT}`);
-});
+connectDB()
+    .then(() => {
+        console.log("Connected to MongoDB");
+
+        app.listen(PORT, () => {
+            console.log(`🚀 Server is running on port ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.error("Error connecting to MongoDB:", error);
+        process.exit(1);
+    });
