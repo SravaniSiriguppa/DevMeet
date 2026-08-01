25c4696ec8446776294b897d5050ba16328e78c2
Sravani <59867313+SravaniSiriguppa@users.noreply.github.com>
2026-08-01 23:43:43 +0530

    feat: hash user passwords during signup

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,11 +1,35 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
 const User = require("../models/User");
 
 const authRouter = express.Router();
 
 authRouter.post("/signup", async (req, res) => {
   try {
-    const user = new User(req.body);
+    const {
+      firstName,
+      lastName,
+      emailId,
+      password,
+      photoUrl,
+      gender,
+      age,
+      about,
+      skills,
+    } = req.body;
+    
+    const passwordHash = await bcrypt.hash(password, 10);
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: passwordHash,
+      photoUrl,
+      gender,
+      age,
+      about,
+      skills,
+    });
     await user.save();
 
     res.status(201).send("User added successfully");
