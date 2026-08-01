0eb844bef490f136fdc3ec6a27a8d2d610af8934
Sravani <59867313+SravaniSiriguppa@users.noreply.github.com>
2026-08-02 00:11:24 +0530

    feat: implement user login endpoint

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -17,7 +17,7 @@ authRouter.post("/signup", async (req, res) => {
       about,
       skills,
     } = req.body;
-    
+
     const passwordHash = await bcrypt.hash(password, 10);
     const user = new User({
       firstName,
@@ -37,5 +37,25 @@ authRouter.post("/signup", async (req, res) => {
     res.status(400).send(err.message);
   }
 });
+
+authRouter.post("/login", async( req, res ) => {
+    try {
+        const { emailId, password } = req.body;
+        const user = await User.findOne({emailId});
+
+        if(!user) {
+            return res.status(404).send("User not found");
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if(!isPasswordValid) {
+            return res.status(401).send("Invalid password");
+        }
+
+        return res.status(200).send("Login successful");
+    } catch(err) {
+        return res.status(400).send(err.message);
+    }
+})
 
 module.exports = authRouter;
