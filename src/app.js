7b720a96af8aacf4b6a2d9ebfb376d72564ec394
Sravani <59867313+SravaniSiriguppa@users.noreply.github.com>
2026-08-01 19:42:51 +0530

    chore: configure development environment

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Welcome to NamasteDev Backend 🚀");
+  res.send("Welcome to NamasteDev Backend 🚀 ");
 });
 
 module.exports = app;
