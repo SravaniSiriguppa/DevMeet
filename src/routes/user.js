c6fdeb3c1951c97bd0273dfbbe49d15f45fa45ce
Sravani <59867313+SravaniSiriguppa@users.noreply.github.com>
2026-08-02 14:07:14 +0530

    feat: implement user connections API

--- src/routes/user.js
+++ src/routes/user.js
@@ -25,4 +25,34 @@ userRouter.get("/requests/received", auth, async (req, res) => {
   }
 });
 
+userRouter.get("/connections", auth, async (req, res) => {
+  const loggedInUser = req.user;
+
+  const connections = await ConnectionRequest.find({
+    $or: [
+      { fromUserId: loggedInUser._id, status: "accepted" },
+      { toUserId: loggedInUser._id, status: "accepted" },
+    ],
+  })
+    .populate(
+      "fromUserId",
+      "firstName lastName photoUrl age gender about skills",
+    )
+    .populate(
+      "toUserId",
+      "firstName lastName photoUrl age gender about skills",
+    );
+
+  const data = connections.map((row) => {
+    if (row.fromUserId._id.equals(loggedInUser._id)) {
+      return row.toUserId;
+    }
+    return row.fromUserId;
+  });
+
+  return res.status(200).json({
+    data,
+  });
+});
+
 module.exports = userRouter;
