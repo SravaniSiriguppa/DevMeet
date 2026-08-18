3bf4eaabb8d43164d2b78b64a03534fdb30413af
Sravani <59867313+SravaniSiriguppa@users.noreply.github.com>
2026-08-18 23:00:32 +0530

    -mongoURI

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,24 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect(process.env.MONGO_URI);
+  // await mongoose.connect(process.env.MONGO_URI);
+  await mongoose
+
+  .connect(process.env.MONGO_URI)
+
+  .then(() => {
+
+    console.log("MongoDB connected");
+
+    console.log("Database:", mongoose.connection.name);
+
+  })
+
+  .catch((err) => {
+
+    console.error("Error connecting to MongoDB:", err);
+
+  });
 };
 
 module.exports = connectDB;
