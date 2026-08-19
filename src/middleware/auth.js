f044af861b7105a12ff5be2079f95b3523b50975
Sravani <59867313+SravaniSiriguppa@users.noreply.github.com>
2026-08-20 00:00:16 +0530

    -debug deployed version

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,12 +4,15 @@ const User = require("../models/User");
 const auth = async( req, res, next) => {
     try {
         const { token } = req.cookies;
+        console.log("COOKIE:", req.cookies);
+        console.log("TOKEN:", req.cookies?.token);
 
         const decoded = jwt.verify(
             token,
             process.env.JWT_SECRET,
         )
 
+        console.log("DECODED:", decoded);
         const user = await User.findById(decoded._id);
 
         if(!user) {
